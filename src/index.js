bdc401445923a55783e7c6e1092a34b817065515
sathishkumar55555 <89385932+sathishkumar55555@users.noreply.github.com>
2023-08-18 23:06:51 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+
+function Page() {
+  return (
+     
+
+     <div>
+   
+    <App/>
+     
+
+     
+     
+     
+     
+     </div>
+
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Page/>
+);
